7db8c0436dd1d0b16f09870a12c91b2bcf454a11
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-12-10 16:13:06 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Inner from "./components/Inner";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Inner />
+    </>
+  );
+}
+
+export default App;
